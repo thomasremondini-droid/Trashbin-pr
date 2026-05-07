f5b33f48a93771d1bf7386937b88fdff63539a01
thomasremondini-droid <thomas.remondini@buonarroti.tn.it>
2026-05-07 14:37:43 +0200

    Aggiornato link Cloudflare

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 
-const URL_RASPBERRY = "https://il-mio-bidone-unico.serveousercontent.com"
+const URL_RASPBERRY = "https://hughes-collections-reservoir-come.trycloudflare.com"
 
 // Funzione per aggiornare i contatori ogni secondo senza ricaricare la pagina
 function updateStats() {
